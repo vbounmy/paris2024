21d522494e13dcb7e8987a9ed8ce14ca3f20bae1
vbounmy <valentine.bounmy@gmail.com>
2024-06-11 01:00:42 +0200

    :boom: fix sites

--- src/App.js
+++ src/App.js
@@ -83,7 +83,7 @@ const App = () => {
                 <Link to="/centres-de-preparation">Centres de préparation</Link>
               </li>
               <li onClick={hideSidebar}>
-                <Link to="/sites-de-competitions">Sites de compétition</Link>
+                <Link to="/sites-de-competitions">Sites de compétitions</Link>
               </li>
               <li onClick={hideSidebar}>
                 <Link to="/parkings-velo">Parkings vélo</Link>
