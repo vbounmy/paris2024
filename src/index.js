83e78d754ec9f36d97b8f2748666511f7b109adc
vbounmy <valentine.bounmy@gmail.com>
2024-06-11 01:47:15 +0200

    :boom: fix route

--- src/index.js
+++ src/index.js
@@ -2,28 +2,13 @@ import "./reset.css";
 import "./index.css";
 import React from "react";
 import ReactDOM from "react-dom";
-import { createHashRouter, RouterProvider } from "react-router-dom";
 import App from "./App.js";
 
-// const root = ReactDOM.createRoot(document.getElementById("root"));
-// root.render(
-//   <React.StrictMode>
-//     <div>
-//       <App />
-//     </div>
-//   </React.StrictMode>
-// );
-
-const router = createHashRouter([
-  {
-    path:"/*",
-    element: <App />,
-  }
-])
-
-ReactDOM.createRoot(document.getElementById("root")).render(
-    <React.StrictMode>
-      <RouterProvider router={router} />
-    </React.StrictMode>
-  );
-
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <div>
+      <App />
+    </div>
+  </React.StrictMode>
+);
