3762721787f630092529935fd7077c11b8b5fd83
vbounmy <valentine.bounmy@gmail.com>
2024-06-11 12:42:58 +0200

    test

--- src/Accueil.js
+++ src/Accueil.js
@@ -3,7 +3,7 @@ import SectionAccueil from "./SectionAccueil.js";
 
 class Accueil extends Component {
   render() {
-    const pageTitle = "Bienvenue";
+    const pageTitle = "Bienvenue !";
     const slogan = "Entrez un lieu et retrouvez les activités à proximité.";
     const content =
       "Vous trouverez toutes les informations facilitant vos déplacements afin de profiter pleinement des Jeux.";
